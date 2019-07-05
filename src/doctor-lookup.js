62b01f27278e02dae8af2f2cdfe1571dc45f053f
jirwincline <jakobinwins@gmail.com>
2019-07-05 12:02:23 -0700

    fix undefined variable in promise and get npm to start live run

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,10 +1,14 @@
 export class Doctor {
   constructor (){
-    this.name = name
-    this.specialty = specialty
+    this.name;
+    this.specialty;
+    this.address;
+    this.phone;
+    this.newPatients;
+
   }
   getDoctorData(){
-      let cityQuery = new Promise(function(resolve,reject){
+      let query = new Promise(function(resolve,reject){
 
         let request = new XMLHttpRequest();
         let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&user_key=14214fdbcf0212819e35f8e2d4aa67fb&name=john`;
@@ -22,8 +26,9 @@ export class Doctor {
         request.send();
       });
       // Promise has to come after the creation of the promise template
-      cityQuery.then(queryPlaceholder => {
-
+      query.then(queryPlaceholder => {
+        let docData = JSON.parse(queryPlaceholder);
+        console.log(docData);
       })
 
     }
