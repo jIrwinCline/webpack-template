f6a60cea9283862bc3021e126af39bdf6cf7844b
jirwincline <jakobinwins@gmail.com>
2019-07-05 23:56:53 -0700

    work on fixing tests

--- spec/doctor-lookup-spec.js
+++ spec/doctor-lookup-spec.js
@@ -18,18 +18,20 @@ describe('Doctor', function () {
   it('should return address, phone, website, and accepting new patient status along with the returning the name', function () {
 
     expect(doctor.address).toBe('123 main st, CA');
-    expect(doctor.address).toBe('5555555555');
-    expect(doctor.address).toBe('www.doc.com');
-    expect(doctor.address).toBe(true);
+    expect(doctor.phone).toBe('5555555555');
+    expect(doctor.website).toBe('www.doc.com');
+    expect(doctor.newPatients).toBe(true);
   });
 
   it('should  return a notification that states what the error is if the API call results in an error (any message not a 200 OK)', function () {
-    expect(doctor.getDoctorData()).toThrowError('API Request Unsuccessful');
+    expect(doctor.getDoctorData()).toThrowError('API Request Unsuccessful');//how do i get this test to fail?
   });
 
   it('should should return a notification that states that no doctors meet the criteria If the query response doesnt include any doctors (for instance, if no doctors meet the search criteria)', function () {
-    let noDoctor = new Doctor();
-    expect(noDoctor.getdoctorData()).toBe('No doctors match your search');
+    let noDoctor = new Doctor('doc');
+    let rsponse = noDoctor.printDocArray();
+    console.log(response)
+    expect(response).toBe('No doctors match your search');
   });
 
 
